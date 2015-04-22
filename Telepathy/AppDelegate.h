5114123bf88c27c09d48c8b9fd1720c550f42c72
Michel Hong <yanshu.hong.michael@gmail.com>
2015-04-22 14:17:37 -0700

    Initial Commit

---
+++ Telepathy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Telepathy
+//
+//  Created by Michael Hong on 4/22/15.
+//  Copyright (c) 2015 Michael Hong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Telepathy/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Telepathy
+//
+//  Created by Michael Hong on 4/22/15.
+//  Copyright (c) 2015 Michael Hong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
